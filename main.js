bea50f3f12870afc4f17ae49355ceeb20450b656
Potewo <potewo@protonmail.com>
2021-04-29 22:05:19 +0900

    it works

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-navigator.clipboard.readText().then(
-  function(clipText) {
-    document.getElementById("text").innerText = clipText;
-  }).catch(
-    error => console.error(error)
-  );
+var qrcode = new QRCode(document.getElementById("qr"), { text: "https://example.com" });
+
+function generateFromClipboard() {
+  navigator.clipboard.readText().then(
+    function(clipText) {
+      generate(clipText);
+    }).catch(
+      error => console.log(error)
+    );
+}
+
+function getCurrentWindowTabs() {
+  return browser.tabs.query({currentWindow: true, active: true});
+}
+
+function generateFromTabURL() {
+  getCurrentWindowTabs().then(tabs => {
+    console.log(tabs);
+    generate(tabs[0].url);
+  }).catch( error => {
+    console.log(error);
+  });
+  // let url = "";
+  // for (let tab of tabs) {
+  //   if (tab.active) {
+  //     url = tab.url;
+  //   }
+  // }
+  // url = tabs[0].url;
+  // generate(url);
+}
+
+function generate(url) {
+  document.getElementById("url").innerText = url;
+  qrcode.clear();
+  qrcode.makeCode(url);
+}
+
+generateFromClipboard();
+// generateFromTabURL();
+
+document.getElementById("fromTabURL").addEventListener('click', generateFromTabURL);
+document.getElementById("fromClipboard").addEventListener('click', generateFromClipboard);
